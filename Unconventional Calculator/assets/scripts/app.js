20165c08828901efd06d8c95ce0f32661d6d88e5
Rishikesh Kale <rvk.rishikeshkale@gmail.com>
2021-06-07 12:29:18 +0530

    Unconventional Calculator Completed

--- Unconventional Calculator/assets/scripts/app.js
+++ Unconventional Calculator/assets/scripts/app.js
@@ -8,21 +8,23 @@ let userHistoryLog = [];
 // Exponentation : **
 
 // Writing Function
-function getUserInput(){
-    // getting userInput from Vendor.js
-    return parseInt(userInput.value);
+function getUserInput() {
+  // getting userInput from Vendor.js
+  return parseInt(userInput.value);
 }
 
-function getUserHistory(operation, num1, num2, result){
-    const logEntry ={
-        operation : operation,
-        num1 : num1,
-        num2 : num2,
-        result : result
-    }
-    userHistoryLog.push(logEntry);
-    console.log(userHistoryLog);
+function getUserHistory(operation, num1, num2, result) {
+  const logEntry = {
+    operation: operation,
+    num1: num1,
+    num2: num2,
+    result: result,
+  };
+  userHistoryLog.push(logEntry);
+  console.log(userHistoryLog);
 }
+
+/* VERSION I
 
 function add(){
     const enteredNumber = getUserInput()
@@ -63,12 +65,65 @@ function divide(){
     getUserHistory("Division", number1, enteredNumber, currentResult);
 }
 
+*/
+
+/* VERSION II */
+
+function calculate(operation) {
+  const enteredNumber = getUserInput();
+
+  if (
+    operation !== "ADD" &&
+    operation !== "SUBTRACTION" &&
+    operation !== "MULTIPLICATION" &&
+    operation !== "DIVISION" ||
+    !enteredNumber
+  ) {
+    return;
+  }
+
+  //if enteredNumber is non-zero, enteredNumber inside if statement would be true 
+
+  const number1 = currentResult;
+  let mathOperator;
+
+  if (operation === "ADD") {
+    currentResult = currentResult + enteredNumber;
+    mathOperator = "+";
+  } else if (operation === "SUBTRACTION") {
+    currentResult = currentResult - enteredNumber;
+    mathOperator = "-";
+  } else if (operation === "MULTIPLICATION") {
+    currentResult = currentResult * enteredNumber;
+    mathOperator = "*";
+  } else if (operation === "DIVISION") {
+    currentResult = currentResult / enteredNumber;
+    mathOperator = "/";
+  }
+
+  let calculationDescription = `${number1} ${mathOperator} ${enteredNumber}`;
+  outputResult(currentResult, calculationDescription);
+  getUserHistory(operation, number1, enteredNumber, currentResult);
+}
+
+function add() {
+  calculate("ADD");
+}
+
+function substract() {
+  calculate("SUBTRACTION");
+}
+
+function multiply() {
+  calculate("MULTIPLICATION");
+}
+
+function divide() {
+  calculate("DIVISION");
+}
+
 //Calling from vendor.js
-addBtn.addEventListener('click', add);
-subtractBtn.addEventListener('click', substract);
-multiplyBtn.addEventListener('click', multiply);
-divideBtn.addEventListener('click', divide);
-
-
-
-
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", substract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
